519ec01ca4cca60f04f7c33a1c700418b5175081
Bastiaan Olij <mux213@gmail.com>
2018-04-16 21:52:31 +1000

    Couple of tweaks

---
+++ Camera_Pivot.gd
@@ -0,0 +1,22 @@
+extends Position3D
+
+var x_angle = 0.0
+var y_angle = 0.0
+var half_pi = PI * 0.5
+var mouse_sensitivity = 0.01
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(1) and event.position.y > 50:
+		x_angle -= event.relative.x * mouse_sensitivity
+		y_angle -= event.relative.y * mouse_sensitivity
+		
+		# limit our up/down angle
+		if y_angle > half_pi:
+			y_angle = half_pi
+		elif y_angle < -half_pi:
+			y_angle = -half_pi
+
+		var new_basis = Basis()
+		new_basis = new_basis.rotated(Vector3(1.0, 0.0, 0.0), y_angle)
+		new_basis = new_basis.rotated(Vector3(0.0, 1.0, 0.0), x_angle)
+		transform.basis = new_basis
--- Main.gd
+++ Main.gd
@@ -5,7 +5,7 @@ var angle = 0
 func _process(delta):
 	angle += delta;
 	
-	$TestCube.transform.basis = Basis(Vector3(0.5, 0.25, 0.1).normalized(), angle)
+	# $TestCube.transform.basis = Basis(Vector3(0.5, 0.25, 0.1).normalized(), angle)
 
 func _ready():
 	$HSlider.value = $Worley_texture.max_distance * 100.0
